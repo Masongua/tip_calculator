50c27e43f34e6df1d445aa86c089fd3505144806
Mason Guaman <masonstalin@gmail.com>
2023-03-07 13:06:00 -0500

    Initial Commit

---
+++ tip_calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tip_calculator
+//
+//  Created by 5e on 3/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
