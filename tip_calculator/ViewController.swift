c8a001e70ba52eb810331e1709cf89c3ba2600ce
Mason Guaman <masonstalin@gmail.com>
2023-04-21 13:04:29 -0400

    ty

--- tip_calculator/ViewController.swift
+++ tip_calculator/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipAmountTextField: UITextField!
     @IBOutlet weak var totalAmountTextField: UITextField!
         
+    @IBOutlet weak var stepper: UIStepper!
     @IBOutlet weak var tip15Button: UIButton!
     @IBOutlet weak var tip18Button: UIButton!
     @IBOutlet weak var tip20Button: UIButton!
@@ -22,52 +23,70 @@ class ViewController: UIViewController {
     @IBOutlet weak var resetButton: UIButton!
     @IBOutlet weak var calculateButton: UIButton!
     @IBOutlet weak var customTipText: UITextField!
+    @IBOutlet weak var amountPerPersonLabel: UILabel!
     
     var tipPercentage: Double = 0.0
+    var totalAmount = 0.0
+    var amountPerPerson = 0.0
 
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            // Do any additional setup after loading the view.
-        }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
         
-        @IBAction func tip15ButtonPressed(_ sender: UIButton) {
-            tipPercentage = 0.15
+    @IBAction func tip15ButtonPressed(_ sender: UIButton) {
+        tipPercentage = 0.15
+        updateTipAmount()
+    }
+        
+    @IBAction func tip18ButtonPressed(_ sender: UIButton) {
+        tipPercentage = 0.18
+        updateTipAmount()
+    }
+        
+    @IBAction func tip20ButtonPressed(_ sender: UIButton) {
+        tipPercentage = 0.2
+        updateTipAmount()
+    }
+        
+    @IBAction func customTipButtonPressed(_ sender: UIButton) {
+        if let customTipText = customTipText.text, let customTip = Double(customTipText) {
+            tipPercentage = customTip / 100
             updateTipAmount()
-        }
-        
-        @IBAction func tip18ButtonPressed(_ sender: UIButton) {
-            tipPercentage = 0.18
-            updateTipAmount()
-        }
-        
-        @IBAction func tip20ButtonPressed(_ sender: UIButton) {
-            tipPercentage = 0.2
-            updateTipAmount()
-        }
-        
-        @IBAction func customTipButtonPressed(_ sender: UIButton) {
-            if let customTipText = customTipText.text, let customTip = Double(customTipText) {
-                tipPercentage = customTip / 100
-                updateTipAmount()
-            }
-        }
-        
-        @IBAction func resetButtonPressed(_ sender: UIButton) {
-            subTotalTextField.text = ""
-            tipAmountTextField.text = ""
-            totalAmountTextField.text = ""
-        }
-        
-        @IBAction func calculateButtonPressed(_ sender: UIButton) {
-            updateTipAmount()
-        }
-        
-        func updateTipAmount() {
-            if let subTotalText = subTotalTextField.text, let subTotal = Double(subTotalText) {
-                let tipAmount = subTotal * tipPercentage
-                let totalAmount = subTotal + tipAmount
-                tipAmountTextField.text = String(format: "%.2f", tipAmount)
-                totalAmountTextField.text = String(format: "%.2f", totalAmount)
-            }
         }
     }
+        
+    @IBAction func resetButtonPressed(_ sender: UIButton) {
+        subTotalTextField.text = ""
+        tipAmountTextField.text = ""
+        totalAmountTextField.text = ""
+        amountPerPersonLabel.text = ""
+    }
+        
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        updateTipAmount()
+    }
+    
+    @IBAction func stepperAction(_ sender: UIStepper) {
+        updateAmountPerPerson()
+    }
+        
+    func updateTipAmount() {
+        if let subTotalText = subTotalTextField.text, let subTotal = Double(subTotalText) {
+            let tipAmount = subTotal * tipPercentage
+            totalAmount = subTotal + tipAmount
+            tipAmountTextField.text = String(format: "%.2f", tipAmount)
+            totalAmountTextField.text = String(format: "%.2f", totalAmount)
+            updateAmountPerPerson()
+        }
+    }
+    
+    func updateAmountPerPerson() {
+        if stepper.value > 0 {
+            amountPerPerson = totalAmount / stepper.value
+            amountPerPersonLabel.text = String(format: "$%.2f per person", amountPerPerson)
+        } else {
+            amountPerPersonLabel.text = ""
+        }
+    }
+}
